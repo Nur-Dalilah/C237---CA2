7e3645bb5f1701a5f7670d09e9cdf85b6f3a1eb6
Nur-Dalilah <milkytae.way@gmail.com>
2025-07-22 17:18:22 +0800

    initialized app.js??

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-hewwo
+const express = require('express');
+const mysql = require('mysql2');
+const session = require('express-session');
+const flash = require('connect-flash');
+const multer = require('multer');
+const app = express();
+
+// Set up multer for file uploads
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images'); // Directory to save uploaded files
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname); 
+    }
+});
+
+const upload = multer({ storage: storage });
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'idk',
+    database: 'idk'
+    //host:'files.io',
+    //port:'files.io',
+    //user:'files.io',
+    //password:'files.io',
+    //database:'files.io'
+  });
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+// Set up view engine
+app.set('view engine', 'ejs');
+//  enable static files
+app.use(express.static('public'));
+// enable form processing
+app.use(express.urlencoded({
+    extended: false
+}));
